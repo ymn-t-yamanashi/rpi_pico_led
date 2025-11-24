93e24d385bd58f869131e14423a1990040fb7266
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-11-24 12:28:02 +0900

    Lチカ実装

--- lib/rpi_pico_led.ex
+++ lib/rpi_pico_led.ex
@@ -2,9 +2,21 @@ defmodule RpiPicoLed do
   @moduledoc """
   Documentation for `RpiPicoLed`.
   """
+  @pin 25
 
   def start do
-    IO.inspect("RpiPicoLed boot")
-    :ok
+    IO.inspect("RpiPicoLed boot Ver 0.03")
+    GPIO.init(@pin)
+    GPIO.set_pin_mode(@pin, :output)
+    loop(@pin, :low)
   end
+
+  defp loop(pin, level) do
+    GPIO.digital_write(pin, level)
+    Process.sleep(100)
+    loop(pin, toggle(level))
+  end
+
+  defp toggle(:high), do: :low
+  defp toggle(:low), do: :high
 end
